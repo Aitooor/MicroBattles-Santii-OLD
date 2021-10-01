45e813bd67adfed14865c9d412250dad66975f1d
santih <imsantih@gmail.com>
2021-10-01 06:47:31 -0300

    Added getters & setters for GameState

--- src/main/java/online/nasgar/microbattles/game/status/GameStatus.java
+++ src/main/java/online/nasgar/microbattles/game/status/GameStatus.java
@@ -2,9 +2,17 @@ package online.nasgar.microbattles.game.status;
 
 public abstract class GameStatus {
 
-    private final GameState gameState;
+    private GameState gameState;
 
     public GameStatus(final GameState gameState) {
+        this.gameState = gameState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(GameState gameState) {
         this.gameState = gameState;
     }
 
