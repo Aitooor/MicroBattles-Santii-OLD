729653b7f10da39ec98f43bb7169991a79f04c56
santih <imsantih@gmail.com>
2021-10-01 06:36:44 -0300

    Added Map object as parameter Added GameStatus to handle all different status of the game

--- src/main/java/online/nasgar/microbattles/game/types/SquadGame.java
+++ src/main/java/online/nasgar/microbattles/game/types/SquadGame.java
@@ -3,15 +3,33 @@ package online.nasgar.microbattles.game.types;
 import online.nasgar.microbattles.MicroBattles;
 import online.nasgar.microbattles.enums.GameType;
 import online.nasgar.microbattles.game.GamePlayer;
+import online.nasgar.microbattles.game.status.GameState;
+import online.nasgar.microbattles.game.status.GameStatus;
+import online.nasgar.microbattles.game.status.list.WaitingStatus;
 import online.nasgar.microbattles.game.team.Team;
 import online.nasgar.microbattles.interfaces.Game;
+import online.nasgar.microbattles.map.Map;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
 
 public class SquadGame extends Game {
 
-    private final Map<UUID, GamePlayer> players = new HashMap<>();
+    private final HashMap<UUID, GamePlayer> players = new HashMap<>();
     private final List<Team> teams = new ArrayList<>();
+    private final Map map;
+    private GameStatus gameStatus;
+
+    public SquadGame(final Map map) {
+        this.map = map;
+        if(map.isEnabled()) {
+            this.gameStatus = new WaitingStatus();
+        }
+    }
+
 
     @Override
     public void addPlayer(final GamePlayer gamePlayer) {
@@ -33,14 +51,21 @@ public class SquadGame extends Game {
 
     @Override
     public void endGame() {
+        map.reset();
+    }
 
+    public Map getMap() {
+        return map;
     }
 
     @Override
-    public Map<UUID, GamePlayer> getPlayers() {
-        return null;
+    public HashMap<UUID, GamePlayer> getPlayers() {
+        return players;
     }
 
+    public GameStatus getNextStatus() {
+        return gameStatus.getNextStatus();
+    }
     @Override
     public void broadcast(String message) {
         super.broadcast(message);
